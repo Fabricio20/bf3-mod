b5bea39294a1c04177d3c3b26ca2c3c6fa5b20a7
Fabricio Winter <git@notfab.net>
2020-12-31 22:58:10 -0300

    Update API

--- ext/Server/WeaponsAPI.lua
+++ ext/Server/WeaponsAPI.lua
@@ -1,15 +1,26 @@
-local WeaponsAPI = { }
-
-function WeaponsAPI.getFiringFunctionData(weaponId, dataId)
+function WeaponsAPI_getFiringFunctionData(weaponId, dataId)
     local fireData = FiringFunctionData(ResourceManager:FindInstanceByGuid(Guid(weaponId), Guid(dataId)))
     fireData:MakeWritable()
     return fireData
 end
 
-function WeaponsAPI.ChangeProjectile(fireData, projectileId)
-	fireData.shot.projectileData:MakeWritable()
-	local newProjectileData = ResourceManager:SearchForInstanceByGuid(Guid(projectileId))
-	fireData.shot.projectileData = ProjectileEntityData(newProjectileData)
+function WeaponsAPI_ChangeProjectile(fireData, projectileId)
+    fireData.shot.projectileData:MakeWritable()
+    local newProjectileData = ResourceManager:SearchForInstanceByGuid(Guid(projectileId))
+    fireData.shot.projectileData = ProjectileEntityData(newProjectileData)
 end
 
-return WeaponsAPI
+--- Returns all instances from a GUID (Usually to get all weapon instances)
+function WeaponsAPI_getAllInstances(partitionId, typeName)
+    local partition = ResourceManager:FindDatabasePartition(Guid(partitionId))
+    local items = {}
+    for _, instance in pairs(partition.instances) do
+        if instance ~= nil then
+            if instance:Is(typeName) then
+                table.insert(items, FiringFunctionData(instance))
+            end
+        end
+    end
+    print('Items found' .. #items)
+    return items
+end
--- ext/Server/notfab.lua
+++ ext/Server/notfab.lua
@@ -11,45 +11,44 @@ end)
 Events:Subscribe('Player:Reload', function(player)
     ChatManager:Yell(player.name .. ' Reloaded', 5.0)
     --print('Weapon: ' .. player.soldier.weaponsComponent.currentWeapon.name)
-    --player.soldier.weaponsComponent.currentWeapon.primaryAmmo = 666
+    player.soldier.weaponsComponent.currentWeapon.primaryAmmo = 666
 
     local projectile = '168F529C-17F6-11E0-8CD8-85483A75A7C5';
 
-    local firingFunctionData_1 = WeaponsAPI:getFiringFunctionData('13D445F7-EBE3-11DF-91EC-895E59A6915B', '4506ABFE-4F34-48BE-A724-C97DA9BA46B4')
-    local firingFunctionData_2 = WeaponsAPI:getFiringFunctionData('13D445F7-EBE3-11DF-91EC-895E59A6915B', 'F25DA02A-4197-4FAF-8503-9E05C705E653')
-
-    WeaponsAPI:ChangeProjectile(firingFunctionData_1, projectile)
-    WeaponsAPI:ChangeProjectile(firingFunctionData_2, projectile)
-
-    firingFunctionData_1.usePrimaryAmmo = true
-    firingFunctionData_2.usePrimaryAmmo = true
-
-    local fireLogic_1 = firingFunctionData_1.fireLogic
-    --fireLogic_1.rateOfFire:MakeWritable()
-    fireLogic_1.rateOfFire = 10000
-
-    local fireLogic_2 = firingFunctionData_2.fireLogic
-    --fireLogic_2.rateOfFire:MakeWritable()
-    fireLogic_2.rateOfFire = 10000
+    -- Firing functions for UMP-45
+    local firingFunctions = WeaponsAPI_getAllInstances('13D445F7-EBE3-11DF-91EC-895E59A6915B', 'FiringFunctionData')
+    if #firingFunctions ~= 0 then
+        for i = 1, #firingFunctions do
+            local data = firingFunctions[i]
+            data:MakeWritable()
+            print('Found instance of a weapon ' .. data.instanceGuid:ToString('D'))
+            WeaponsAPI_ChangeProjectile(data, projectile)
+            print('Changed Projectile')
+            -- Slower RoF
+            local fireLogic = data.fireLogic
+            fireLogic.rateOfFire = 10000
+            print(fireLogic.rateOfFire)
+        end
+    end
 end)
 
 Events:Subscribe('Player:Killed', function(player)
     print(player.name .. ' was Killed')
-	ChatManager:Yell(player.name .. ' was Killed', 1.0)
+    ChatManager:Yell(player.name .. ' was Killed', 1.0)
 end)
 
 Events:Subscribe('Player:Destroy', function(player)
     print(player.name .. ' was Destroyed')
-	ChatManager:Yell(player.name .. ' was Destroyed', 3.0)
+    ChatManager:Yell(player.name .. ' was Destroyed', 3.0)
 end)
 
-Events:Subscribe('Player:ReviveRefused', function(player) 
+Events:Subscribe('Player:ReviveRefused', function(player)
     local players = PlayerManager:GetPlayersByTeam(player.teamId)
     if #players ~= 0 then
         for i = 1, #players do
             local target = players[i]
             if target.alive and target.guid ~= player.guid then
-                print('Respawning on '..target.name)
+                print('Respawning on ' .. target.name)
 
                 local transform = LinearTransform()
 
