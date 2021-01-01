57a6e9cdabd1e3fd3f15ec6ae2a3e20a4e42df8f
Fabricio Winter <git@notfab.net>
2021-01-01 00:44:04 -0300

    Glock mods

--- ext/Server/notfab.lua
+++ ext/Server/notfab.lua
@@ -11,9 +11,7 @@ end)
 Events:Subscribe('Player:Reload', function(player)
     ChatManager:Yell(player.name .. ' Reloaded', 5.0)
     --print('Weapon: ' .. player.soldier.weaponsComponent.currentWeapon.name)
-    player.soldier.weaponsComponent.currentWeapon.primaryAmmo = 666
-
-    local projectile = '168F529C-17F6-11E0-8CD8-85483A75A7C5';
+    --player.soldier.weaponsComponent.currentWeapon.primaryAmmo = 666
 
     -- Firing functions for UMP-45
     local firingFunctions = WeaponsAPI_getAllInstances('13D445F7-EBE3-11DF-91EC-895E59A6915B', 'FiringFunctionData')
@@ -22,12 +20,25 @@ Events:Subscribe('Player:Reload', function(player)
             local data = firingFunctions[i]
             data:MakeWritable()
             print('Found instance of a weapon ' .. data.instanceGuid:ToString('D'))
-            WeaponsAPI_ChangeProjectile(data, projectile)
+            WeaponsAPI_ChangeProjectile(data, '168F529C-17F6-11E0-8CD8-85483A75A7C5')
             print('Changed Projectile')
+            data.ammo.magazineCapacity = 4000
             -- Slower RoF
             local fireLogic = data.fireLogic
-            fireLogic.rateOfFire = 10000
+            fireLogic.rateOfFire = 600
             print(fireLogic.rateOfFire)
+        end
+    end
+
+    local firingsGlock18 = WeaponsAPI_getAllInstances('3B3F9879-EB4B-11DF-8AA6-AE0344995412', 'FiringFunctionData')
+    if #firingsGlock18 ~= 0 then
+        for i = 1, #firingsGlock18 do
+            local data = firingsGlock18[i]
+            data:MakeWritable()
+            print('Found instance of a weapon ' .. data.instanceGuid:ToString('D'))
+            WeaponsAPI_ChangeProjectile(data, 'CEC6D381-72DE-B7D4-E998-0D566E0575C6')
+            print('Changed Projectile')
+            data.ammo.magazineCapacity = 21
         end
     end
 end)
